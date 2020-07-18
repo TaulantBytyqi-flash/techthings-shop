b5b422d3fe0d7e47d1cc4dabde8b3ad4f03dcec6
TaulantBytyqi-flash <tb41546@ubt-uni.net>
2020-07-18 21:57:18 +0200

    auth fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -84,22 +84,3 @@ exports.signinController = async (req, res) => {
 
 };
 
-
-let auth = (req, res, next) => {
-    let token = req.cookies.w_auth;
-  
-    User.findByToken(token, (err, user) => {
-      if (err) throw err;
-      if (!user)
-        return res.json({
-          isAuth: false,
-          error: true
-        });
-  
-      req.token = token;
-      req.user = user;
-      next();
-    });
-  };
-
-module.exports = { auth }; 
